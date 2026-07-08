f818cb070844243b6997ae1b4bcf669ff01c843e
riddhima021 <riddhima0721@gmail.com>
2026-07-09 00:36:05 +0530

    Completed authentication module with JWT middleware

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const router = express.Router();
+
+const authMiddleware = require("../middleware/authMiddleware");
+
+const { getProfile } = require("../controllers/userController");
+
+// Protected Route
+router.get("/profile", authMiddleware, getProfile);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
@@ -13,6 +14,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
     res.send("SpeakSmart AI Backend Running 🚀");
